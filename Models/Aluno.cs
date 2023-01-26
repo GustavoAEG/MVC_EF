531e7f15afd63c02aa8d31e9d1960cb2a08d2d71
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-01-25 23:36:43 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Aluno.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_EF.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public string Email { get; set; }
+        public DateTime Nascimento { get; set; }
+    }
+}
